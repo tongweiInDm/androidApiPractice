a9e75f48c23e6c4ebbeae35bf4b17b2ac1b4948b
tongwei <tongwei@xiaomi.com>
2019-03-03 17:42:19 +0800

    read dimension from custom attr

--- api/customeViewAndGroup/app/src/main/java/com/tw/customeviewandgroup/customview/ReadCustomAttrView.java
+++ api/customeViewAndGroup/app/src/main/java/com/tw/customeviewandgroup/customview/ReadCustomAttrView.java
@@ -16,12 +16,21 @@ public class ReadCustomAttrView extends View {
         super(context, attrs);
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.AttrView);
         int int_value_test1= typedArray.getInt(R.styleable.AttrView_test1,0);
+        int int_value_test2= typedArray.getInt(R.styleable.AttrView_test2,0);
         boolean boolean_value_test1= typedArray.getBoolean(R.styleable.AttrView_test1,false);
 
         Log.d(TAG, "read custom int value of test1 " + int_value_test1);
+        Log.d(TAG, "read custom int value of test2 " + int_value_test2);
         Log.d(TAG, "read custom boolean value of test1 " + boolean_value_test1);
-
         typedArray.recycle();
+
+        TypedArray typedArrayMea = context.obtainStyledAttributes(attrs, R.styleable.AttrView_measure);
+        float float_value_test3 = typedArrayMea.getDimension(R.styleable.AttrView_measure_test3,0);
+
+        Log.d(TAG, "read custom float value of test3 " + float_value_test3);
+
+        typedArrayMea.recycle();
+
     }
 
     public ReadCustomAttrView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
